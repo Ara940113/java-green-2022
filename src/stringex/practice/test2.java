36ac4739b8bcba6c3f97105352f05f9eec1a962d
AR <aki1013@naver.com>
2022-01-20 01:03:24 +0900

    java

--- src/stringex/practice/test2.java
+++ src/stringex/practice/test2.java
@@ -2,11 +2,20 @@ package stringex.practice;
 
 import java.util.Scanner;
 
+import javax.print.Doc;
+
 class 개인정보 {
     private String name; // 이름
     private String tel; // 전화번호
     private char gender; // 성별
     private int age; // 나이
+
+    public 개인정보(String name, String tel, char gender, int age) {
+        this.name = name;
+        this.tel = tel;
+        this.gender = gender;
+        this.age = age;
+    }
 
     public String getName() {
         return name;
@@ -47,25 +56,26 @@ public class test2 {
 
         String data = sc.nextLine();
 
-        String[] tokens = data.split("-");
+        String[] tokens = data.split("-|:");
 
-        if (tokens[0] == "A01") {
+        if (tokens[0].equals("A01")) {
             for (int i = 1; i < 5; i++) {
-                System.out.println(tokens[i]);
+                // System.out.println(tokens[i]);
             }
         } else {
             System.out.println("프로그램 종료");
         }
 
-        String name = tokens[1];
-        String tel = tokens[2];
-        // char gender = tokens[3];
-        int age = Integer.parseInt(tokens[4]);
-        // tokens[4]=age.trim();
+        String name = tokens[2];
+        String tel = tokens[4];
+        char gender = tokens[6].charAt(0);
+        int age = Integer.parseInt(tokens[8].trim());
 
-        개인정보 doc1 = new 개인정보(
+        개인정보 doc1 = new 개인정보(name, tel, gender, age);
 
-        );
-
+        System.out.println("이름은 " + doc1.getName() + " 입니다");
+        System.out.println("전화번호는 " + doc1.getTel() + " 입니다");
+        System.out.println("성별은 " + doc1.getGender() + " 입니다");
+        System.out.println("나이는 " + doc1.getAge() + " 입니다");
     }
 }
